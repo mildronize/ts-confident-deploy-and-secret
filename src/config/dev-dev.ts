b337cd1745d92932ddb79f865028fc76e289b914
Thada Wangthammang <thada.wth@gmail.com>
2026-01-09 22:21:15 +0700

    Refactor Azure container app configuration for clarity and consistency

--- src/config/dev-dev.ts
+++ src/config/dev-dev.ts
@@ -1,22 +1,25 @@
 import type { MatrixConfigBase } from "../libs/types";
 
+// TODO: Update to your own values
+const displayName = 'mildr';
+
 export const devResources: Record<string, MatrixConfigBase[]> = {
   'azure_container_app': [
     {
       id: 'dev-dev-001',
       type: 'azure_container_app',
-      name: 'e2e-type-safety-deploy-and-secret-dev-dev-001',
-      resource_group: 'e2e-type-safety-deploy-and-secret-rg-dev',
+      name: `app-ntotr-${displayName}`,
+      resource_group: 'rg-northern-tech-workshop',
       container_image: 'ghcr.io/mildronize/kubricate-demo-azure-global-2025:main',
       credential: {
         type: 'key_vault',
         gh_secret_name: 'AZURE_CREDENTIALS_DEV',
-        vault_name: 'e2e-type-safety-deploy-and-secret-kv-dev',
-        secret_name: 'e2e-type-safety-deploy-and-secret-sp-dev-001',
+        vault_name: 'kv-ntotr-shared',
+        secret_name: `sp-azure-container-app-app-ntotr-${displayName}`,
       },
       metadata: {
-        subscription_id: '973c8d8f-3f1c-4d92-9e1a-6f2b12f0b2e7',
-        service_principal_name: 'e2e-type-safety-deploy-and-secret-sp-dev-001',
+        subscription_id: '0c249ac1-38ac-4cb4-a429-8b1448de6d8e',
+        service_principal_name: `sp-ntotr-azure-container-app-app-ntotr-${displayName}`,
       },
     },
   ]
